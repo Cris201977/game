ea9229503e109f08fd8bb331cdeb489dcd1020b4
Cris201977 <cris.iban2016@gmail.com>
2023-12-14 12:52:33 -0300

    14 dic - 13hs

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,23 +1,31 @@
 import './App.css'
 import Mensaje from './componenentes/Mensaje'
 import { useState } from 'react'
+import preguntas from './data/preguntas.json'
+import  Opcion  from './componenentes/Opcion'
+import respuestas from './data/respuestas.json'
 
 function App() {
-const [texto, setText] = useState("Apretá el botón para ver la pregunta")
-
-  fetch(_raw)
-  .then(r => r.text())
-  .then(text => { setRaw(text) });
+const [texto, setTexto] = useState("Apretá el botón para ver la pregunta")
+const [op1, setOp1] = useState("Opción 1")
+const [op2, setOp2] = useState("Opción 2")
+const max = 3;
 
   return (
     <>
-<Mensaje mensaje={texto}/>
+
+<Mensaje mensaje={texto} />
 <button onClick={()=>{
-  setText("Cargando pregunta...")
+let num = (Math.floor(Math.random() * max)+1).toString();
+let numPreg = (Math.floor(Math.random() * max)+1).toString();
   setTimeout(()=>{
-      setText("./preguntas/p-1.txt")
+setTexto(preguntas[0][3].pregunta)
+setOp1(respuestas[0].pregunta_1[0][numPreg].opcion1[0])
+setOp2(respuestas[0].pregunta_1[0][numPreg].opcion2[0])
   }, 2000)
 }}>Jugar</button>
+<Opcion opcion={op1}/>
+<Opcion opcion={op2}/>
     </>
   )
 }
---
+++ game/src/componenentes/Opcion.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Opcion(props) {
+  return (
+    <div>
+      <button>{props.opcion}</button>
+    </div>
+  )
+}
+
+export default Opcion
+
